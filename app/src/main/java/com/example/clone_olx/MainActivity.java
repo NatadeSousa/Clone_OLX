6e8882d7e71ca929b5922ae130e1155448ec0a2c
Ins1ghtful <natan345894@gmail.com>
2022-06-29 21:04:17 -0300

    Toolbar da MainActivity pronta

--- app/src/main/java/com/example/clone_olx/MainActivity.java
+++ app/src/main/java/com/example/clone_olx/MainActivity.java
@@ -3,20 +3,42 @@ package com.example.clone_olx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    //
+    private ImageButton ibGetBack;
+
+    //Activity Life Cycles
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        doIt();
+        referComponents();
+        setClicks();
+        validateData();
 
     }
+    //-----------------------------------------------------------------
 
-    private void doIt(){
+
+    //Validating data that user typed
+    private void validateData(){
 
     }
+    //-----------------------------------------------------------------
+
+    //Setting clicks on buttons
+    private void setClicks(){
+
+    }
+    //-----------------------------------------------------------------
+    
+    //Referring components
+    private void referComponents(){
+        ibGetBack = findViewById(R.id.ib_get_back);
+    }
+    //-----------------------------------------------------------------
+
 }
