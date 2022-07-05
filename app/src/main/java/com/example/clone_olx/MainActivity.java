818f8dc1d4137a00605fa95f8d355ad6f6b99100
Natã de Sousa de Paula <natan345894@gmail.com>
2022-07-05 15:11:24 -0300

    ...

--- app/src/main/java/com/example/clone_olx/MainActivity.java
+++ app/src/main/java/com/example/clone_olx/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.clone_olx;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         referComponents();
         setClicks();
