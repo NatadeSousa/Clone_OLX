3d98f4bc607d0f0c3f5d597a72345a2de3025eda
Natã de Sousa de Paula <natan345894@gmail.com>
2022-07-06 18:50:46 -0300

    Layout da activity MyAdress configurado

--- app/src/main/java/com/example/clone_olx/Activity/MyAddressActivity.java
+++ app/src/main/java/com/example/clone_olx/Activity/MyAddressActivity.java
@@ -3,14 +3,62 @@ package com.example.clone_olx.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.example.clone_olx.R;
 
 public class MyAddressActivity extends AppCompatActivity {
 
+    private Button btnSave;
+    private ImageButton ibGetBack;
+    private ProgressBar pbMyAddressActivity;
+    private EditText editCep,editUf,editCity,editNeighborhood;
+
+    //Activity Life Cycles
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_adress);
+        setContentView(R.layout.activity_my_address);
+
+        referComponents();
+        setClicks();
+
     }
+    //--------------------------------------------------------------------
+
+    //Validating data provided by current user
+    private void validateData(){
+
+    }
+    //--------------------------------------------------------------------
+
+    //Setting clicks on Buttons
+    private void setClicks(){
+        btnSave.setOnClickListener(view -> {
+            btnSave.setVisibility(View.INVISIBLE);
+            pbMyAddressActivity.setVisibility(View.VISIBLE);
+
+            validateData();
+        });
+
+        ibGetBack.setOnClickListener(view -> finish());
+    }
+    //--------------------------------------------------------------------
+
+    //Referring components
+    private void referComponents(){
+        btnSave = findViewById(R.id.btn_save);
+        ibGetBack = findViewById(R.id.ib_get_back);
+        pbMyAddressActivity = findViewById(R.id.pb_my_address_activity);
+        editCep = findViewById(R.id.edit_cep);
+        editNeighborhood = findViewById(R.id.edit_neighborhood);
+        editCity = findViewById(R.id.edit_city);
+        editUf  = findViewById(R.id.edit_uf);
+    }
+    //--------------------------------------------------------------------
+
 }
--- app/src/main/java/com/example/clone_olx/Fragment/MyAccountFragment.java
+++ app/src/main/java/com/example/clone_olx/Fragment/MyAccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.clone_olx.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.clone_olx.Activity.MyAddressActivity;
+import com.example.clone_olx.Activity.MyProfileActivity;
 import com.example.clone_olx.R;
 
 public class MyAccountFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_account, container, false);
+
+        setClicks(view);
+
+        return view;
     }
+
+    //Setting clicks on options
+    private void setClicks(View view){
+        view.findViewById(R.id.my_profile).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), MyProfileActivity.class));
+        });
+
+        view.findViewById(R.id.my_address).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), MyAddressActivity.class));
+        });
+    }
+    //--------------------------------------------------------------------
 }
