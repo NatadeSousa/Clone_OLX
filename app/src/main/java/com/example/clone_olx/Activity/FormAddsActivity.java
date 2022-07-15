878b88379fa3c967300b98d98cf1b64b7d26dd23
Natã de Sousa de Paula <natan345894@gmail.com>
2022-07-15 11:55:27 -0300

    Algumas configurações foram adicionadas à Activity FormAdds
    Novas EditText's e TextView's foram adicionadas. Efeito quando o usuário clicar no botão Registrar Anúncio foi configurado.

--- app/src/main/java/com/example/clone_olx/Activity/FormAddsActivity.java
+++ app/src/main/java/com/example/clone_olx/Activity/FormAddsActivity.java
@@ -3,8 +3,12 @@ package com.example.clone_olx.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.clone_olx.R;
@@ -13,6 +17,7 @@ public class FormAddsActivity extends AppCompatActivity {
 
     private ImageButton ibGetBack;
     private Button btnCreateAdd;
+    private ProgressBar pbFormAddsActivity;
 
     //Activity Life Cycle
     @Override
@@ -30,18 +35,27 @@ public class FormAddsActivity extends AppCompatActivity {
     private void setClicks(){
 
         btnCreateAdd.setOnClickListener(view -> {
-            Toast.makeText(this, "Anúncio Salvo!", Toast.LENGTH_SHORT).show();
+            btnCreateAdd.setVisibility(View.INVISIBLE);
+            pbFormAddsActivity.setVisibility(View.VISIBLE);
+
+            new Handler(Looper.getMainLooper()).postDelayed(this::showMessage, 2000);
         });
         ibGetBack.setOnClickListener(view -> finish());
 
     }
     //--------------------------------------------------------------------------------------
+    private void showMessage(){
+        Toast.makeText(this, "Dados salvos!", Toast.LENGTH_SHORT).show();
+        pbFormAddsActivity.setVisibility(View.GONE);
+        btnCreateAdd.setVisibility(View.VISIBLE);
+    }
 
     //Referring components
     private void referComponents(){
 
         btnCreateAdd = findViewById(R.id.btn_create_add);
         ibGetBack = findViewById(R.id.ib_get_back);
+        pbFormAddsActivity = findViewById(R.id.pb_form_adds_activity);
 
     }
     //--------------------------------------------------------------------------------------
