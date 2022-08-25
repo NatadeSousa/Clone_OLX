488fd8bed050274f94277a4cbb357915401c75ab
Natã de Sousa <natan345894@gmail.com>
2022-08-24 21:41:44 -0300

    Layout da Activity AddDetails configurado

---
+++ app/src/main/java/com/example/clone_olx/Activity/FragmentHome/AddDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.clone_olx.Activity.FragmentHome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clone_olx.R;
+
+public class AddDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_details);
+    }
+}
---
+++ app/src/main/java/com/example/clone_olx/Adapter/AdapterSlider.java
@@ -0,0 +1,49 @@
+package com.example.clone_olx.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.clone_olx.R;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterSlider extends SliderViewAdapter<AdapterSlider.AdapterSliderVH> {
+
+    private final List<String> imagesUrls;
+
+    public AdapterSlider(List<String> imagesUrls) {
+        this.imagesUrls = imagesUrls;
+    }
+
+    @Override
+    public AdapterSliderVH onCreateViewHolder(ViewGroup parent) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_slider, null);
+        return new AdapterSliderVH(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(AdapterSliderVH viewHolder, int position) {
+        String imageUrl = imagesUrls.get(position);
+        Picasso.get().load(imageUrl).into(viewHolder.imageViewBackground);
+    }
+
+    @Override
+    public int getCount() {
+        return imagesUrls.size();
+    }
+
+    static class AdapterSliderVH extends SliderViewAdapter.ViewHolder {
+
+        ImageView imageViewBackground;
+
+        public AdapterSliderVH(View itemView) {
+            super(itemView);
+            imageViewBackground = itemView.findViewById(R.id.image_add);
+        }
+    }
+
+}
